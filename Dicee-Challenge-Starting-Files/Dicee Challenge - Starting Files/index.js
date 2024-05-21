9c9c2336ff800fe959b57d5d24eb8a1ac300a66d
Musfiqur Rahman Zihad <musfiqurahmanzihad@gmail.com>
2024-05-22 01:10:59 +0600

    Change the text refresh now to draw

--- Dicee-Challenge-Starting-Files/Dicee Challenge - Starting Files/index.js
+++ Dicee-Challenge-Starting-Files/Dicee Challenge - Starting Files/index.js
@@ -5,9 +5,9 @@ var imageOne = document.querySelector(".img1").setAttribute("src", "./images/dic
 var imageTwo = document.querySelector(".img2").setAttribute("src", "./images/dice"+ randomNumberTwo +".png")
 
 if(randomNumberOne === randomNumberTwo) {
-    document.querySelector("h1").textContent = "Refresh Again";
+    document.querySelector("h1").textContent = "💪Draw!";
 } else if(randomNumberOne > randomNumberTwo) {
-    document.querySelector("h1").textContent = "🥇Player 1 Wins";
+    document.querySelector("h1").textContent = "🏆Player 1 Wins";
 } else{
-    document.querySelector("h1").textContent = "🥇Player 2 Wins";
+    document.querySelector("h1").textContent = "🏆Player 2 Wins";
 }
