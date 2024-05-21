16ea471d3d109d63c2f88192eea8c2ea45908798
Musfiqur Rahman Zihad <musfiqurahmanzihad@gmail.com>
2024-05-21 23:10:30 +0600

    updated my name with javascript

--- DOM-Challenge-Starting-Files/DOM Challenge Starting Files/index.js
+++ DOM-Challenge-Starting-Files/DOM Challenge Starting Files/index.js
@@ -1,1 +1,5 @@
 document.querySelector("h1").innerHTML = "Good Bye"
+document.querySelector("input").click()
+document.querySelector("li").style.color = "red"
+document.querySelector("h1").style.color = "blue"
+document.querySelector("ul").lastElementChild.innerHTML="Musfiqur Rahman"
